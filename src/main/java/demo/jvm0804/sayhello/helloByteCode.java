02f66c39e50fe2ff9fadf81320c3bb6ec18f2636
jinzhuwei <zhuwei.jin@ai-ym.com>
2021-08-08 23:53:51 +0800

    第一周，ClassLoader

---
+++ src/main/java/demo/jvm0804/sayhello/helloByteCode.java
@@ -0,0 +1,9 @@
+package demo.jvm0804.sayhello;
+
+public class helloByteCode {
+    public static void main(String[] args){
+        String aa = "helloByteCode";
+        System.out.println(aa);
+        helloByteCode obj = new helloByteCode();
+    }
+}
